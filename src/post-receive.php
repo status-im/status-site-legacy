1f0c95e40d028e649a4f8eb2d5cd5de5d7478471
Adrian Tiberius <adrian.tiberius@gmail.com>
2016-09-18 09:37:27 +0300

    add deploy debug logs

--- src/post-receive.php
+++ src/post-receive.php
@@ -2,13 +2,16 @@
 $command = "cd /var/www/status-site && git reset --hard HEAD && git pull origin master && gulp build";
 
 // TODO check against ip or secret hash
-
+$log_file = "/var/www/status-site/build.log";
+file_put_contents($log_file, var_export($_SERVER, true), FILE_APPEND);
+$content = file_get_contents("php://input");
+file_put_contents($log_file, var_export($content, true), FILE_APPEND);
 if ( $_SERVER['X-GitHub-Event'] == 'push' ) {
 
     if( ($fp = popen($command, "r")) ) {
         while( !feof($fp) ){
             $result = fread($fp, 1024);
-            file_put_contents("/var/www/status-site/build.log", $result, FILE_APPEND);
+            file_put_contents($log_file, $result, FILE_APPEND);
             echo $result;
             flush();
         }
