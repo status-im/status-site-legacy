e1e029f03308cb871f6f64b76e9ef66148712d07
Adrian Tiberius <adrian.tiberius@gmail.com>
2016-09-18 08:16:17 +0300

    fix waitlist.html maitre links

--- src/post-receive.php
+++ src/post-receive.php
@@ -8,7 +8,7 @@ if ( $_SERVER['HTTP_X_GITHUB_EVENT'] == 'push' ) {
     if( ($fp = popen($command, "r")) ) {
         while( !feof($fp) ){
             $result = fread($fp, 1024);
-            file_put_contents("build.log", $result, FILE_APPEND);
+            file_put_contents("/var/www/status-site/build.log", $result, FILE_APPEND);
             echo $result;
             flush();
         }
