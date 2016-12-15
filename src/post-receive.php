7616decdf9ea2c4281aabdee6200f0567261f0eb
Adrian Tiberius <adrian.tiberius@gmail.com>
2016-12-15 07:53:18 +0200

    remove extra debug messages

--- src/post-receive.php
+++ src/post-receive.php
@@ -5,9 +5,6 @@ $command = "cd /var/www/status-site && git reset --hard HEAD && git pull origin 
 
 // TODO check against ip or secret hash
 $log_file = "/var/www/status-site/build.log";
-file_put_contents($log_file, var_export($_SERVER, true), FILE_APPEND);
-$content = file_get_contents("php://input");
-file_put_contents($log_file, var_export($content, true), FILE_APPEND);
 if ( $_SERVER['HTTP_X_GITHUB_EVENT'] == 'push' ) {
 
     if( ($fp = popen($command, "r")) ) {
