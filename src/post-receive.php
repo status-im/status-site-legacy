0e94d4d5204001a351498c37d080a30747a103d9
Adrian Tiberius <adrian.tiberius@gmail.com>
2016-12-15 08:20:06 +0200

    test deploy

--- src/post-receive.php
+++ src/post-receive.php
@@ -1,7 +1,7 @@
 <?php
 ignore_user_abort(true);
 set_time_limit(0);
-$command = "cd /var/www/status-site && git reset --hard HEAD && git pull origin master && npm install && gulp build";
+$command = "cd /var/www/status-site && git reset --hard HEAD && git pull origin master && npm install && gulp build 2>&1";
 
 // TODO check against ip or secret hash
 $log_file = "/var/www/status-site/build.log";
@@ -11,8 +11,8 @@ if ( $_SERVER['HTTP_X_GITHUB_EVENT'] == 'push' ) {
         while( !feof($fp) ){
             $result = fread($fp, 1024);
             file_put_contents($log_file, $result, FILE_APPEND);
-            echo $result;
-            flush();
+            //echo $result;
+            //flush();
         }
         fclose($fp);
     }
