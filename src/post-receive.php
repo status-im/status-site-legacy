1c741273c423dfbfd88468c036df3e594e979e2d
Adrian Tiberius <adrian.tiberius@gmail.com>
2016-12-15 10:28:14 +0200

    fix npm install exec

--- src/post-receive.php
+++ src/post-receive.php
@@ -1,19 +1,9 @@
 <?php
 ignore_user_abort(true);
 set_time_limit(0);
-$command = "cd /var/www/status-site && git reset --hard HEAD && git pull origin master && npm install && gulp build 2>&1";
 
 // TODO check against ip or secret hash
-$log_file = "/var/www/status-site/build.log";
 if ( $_SERVER['HTTP_X_GITHUB_EVENT'] == 'push' ) {
-
-    if( ($fp = popen($command, "r")) ) {
-        //while( !feof($fp) ){
-        //    $result = fread($fp, 1024);
-            file_put_contents($log_file, $fp, FILE_APPEND);
-            //echo $result;
-            //flush();
-        //}
-        fclose($fp);
-    }
+  shell_exec("sudo /var/www/status-site/deploy.sh");
+  echo file_get_contents("/var/www/status-site/build.log");
 }
